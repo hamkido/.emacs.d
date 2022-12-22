1e228ad3b649566bc8999323d2436560de8a8c53
hamki <hamki.do2000@gmail.com>
2022-12-22 21:32:15 +0800

    rm some config

--- init.el
+++ init.el
@@ -10,37 +10,22 @@
   (package-refresh-contents)
   )
 (setq my-package-list '(use-package))
-
-;; programmatically install/ensure installed
-;; pkgs in your personal list
 (dolist (package my-package-list)
   (unless (package-installed-p package)
     (package-install package)))
-
 (org-babel-load-file "~/.emacs.d/emacs.org")
-					;(load "~/.emacs.d/editor.el")
-					;(load "~/.emacs.d/time.el")
-					;(load "~/.emacs.d/data.el")
-					;(load "~/.emacs.d/programming.el")
-					;(load "~/var/variable.el")
-
 ;;;; config
 (set-default-coding-systems 'utf-8)
 (server-start)
-					;'(helm-minibuffer-history-key "M-p")
-					;'(emacs-everywhere-frame-parameters '((name . "emacs-everywhere") (width . 120) (height . 80)))
 ;;;; custom
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "/Users/hamkihan/.emacs.d/bookmarks")
  '(org-blank-before-new-entry '((heading) (plain-list-item)))
  '(org-habit-graph-column 80)
  '(org-tags-column -30)
- '(package-selected-packages
-   '(flyspell-correct flyspell-popup register-list iregister register-channel register-quicknav org-tanglesync org-ql))
  '(window-min-height 6)
  '(window-min-width 32))
 (custom-set-faces
